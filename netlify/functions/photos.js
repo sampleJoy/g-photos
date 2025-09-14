c9ae82cc1a2ee5192cd63ac355ade3ce47f40322
sampleJoy <108215225+sampleJoy@users.noreply.github.com>
2025-09-14 11:21:24 +0530

    Update photos.js

--- netlify/functions/photos.js
+++ netlify/functions/photos.js
@@ -1,7 +1,7 @@
 // netlify/functions/photos.js
 const GooglePhotosAlbum = require("google-photos-album-image-url-fetch");
 
-const ALBUM_LINK = "https://photos.app.goo.gl/yourAlbumLinkHere"; // <-- replace with your album
+const ALBUM_LINK = "https://photos.app.goo.gl/TBdqFucP8dKTDR2n9"; // <-- replace with your album
 
 exports.handler = async function (event, context) {
   try {
